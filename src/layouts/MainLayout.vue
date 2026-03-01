fabe46409e4301172ac41a1c669531878806cb06
NguyennMinhh <minhnguyen8762382004@gmail.com>
2026-03-01 21:53:32 +0700

    feat: restructure app layout and add routing
    - Updated App.vue to use MainLayout and router-view for better structure.
    - Created MainLayout.vue for consistent layout with a header and slot for content.
    - Added HomeView.vue for the home page and UserDetail.vue for user details.
    - Configured router to include routes for home and user detail views.
    - Removed unused counter store to clean up the codebase.

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,24 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="panel">
+        <div class="panel-header">
+            <router-link to="/">Home</router-link>
+            <p>User Dashboard</p>
+        </div>
+
+        <div class="panel-body">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.panel-header {
+    display: flex;
+    flex-direction: row;
+    gap: 1rem;
+}
+</style>
---
+++ src/views/UserDetail.vue
@@ -0,0 +1,8 @@
+<script setup></script>
+
+<template>
+    <div>User Detail</div>
+</template>
+
+<style scoped>
+</style>
